11824e1cdc292aa4da2f20469c8557608c1f01c4
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-01-30 17:41:32 -0600

    added stub for orchestrator, experimentally synchronous

---
+++ AlgaeTMorch/AlgaeTMorch.ino
@@ -0,0 +1,102 @@
+#include <RS485_protocol.h>
+#include <SoftwareSerial.h>
+#include <EEPROM.h>
+
+//Software Serial for Local CO2 Sensor:
+#define SENSOR_TX_PIN 11
+#define SENSOR_RX_PIN 10
+SoftwareSerial mySerial(SENSOR_RX_PIN, SENSOR_TX_PIN); // RX, TX
+
+//Software Serial for RS485 network:
+#define RS485_ENABLE_PIN 4
+#define RS485_TX_PIN 3
+#define RS485_RX_PIN 2
+SoftwareSerial rs485 (RS485_RX_PIN, RS485_TX_PIN);  // RX, TX
+void fWrite (const byte what){rs485.write(what);}
+int fAvailable (){return rs485.available ();}
+int fRead (){return rs485.read ();}
+
+//Pump stuff:
+#define PUMP_RELAY_1 9
+#define PUMP_RELAY_2 8
+bool PUMP1 = false;
+bool PUMP2 = false;
+int READING = 0;
+int RATE = 0;
+
+#define LED_PIN 11
+
+byte myAddress = 0;
+
+const byte GRACE_PERIOD = 24; //how many times we let a node fail before we stop polling it
+
+const byte KnownInterns = 4;
+byte internReadings[] = {0,0,0,0};
+byte deadNodes[] = {0,0,0,0};
+byte probation[KnownInterns];
+
+void setup() {
+  myAddress = EEPROM.read (0);
+  rs485.begin (38400);
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode (RS485_ENABLE_PIN, OUTPUT);
+  pinMode (PUMP_RELAY_1, OUTPUT);
+  pinMode (PUMP_RELAY_2, OUTPUT);
+}
+
+void loop() {
+    //constantly loop over intern sensors, querying each for a response.
+  for (int i=1; i <= KnownInterns; i++){
+    if(deadNodes[i-1] == 1){ //if we reach the id of a dead node, skip it so the whole deal doesn't slow down.
+      continue; //TODO: how does a node recover from being blacklisted here? Implement a recovery timer with millis() to operate in concert with a 
+      //no-contact timer on the intern end so that we go Normal Op->Fail 25 Reports->BlackList->No Contact for 30 seconds->Intern reboots->attempt contact again after 35 seconds
+    }
+    byte msg [] = { 
+      i    // device 1
+    };
+    digitalWrite (LED_PIN, HIGH);
+    digitalWrite (RS485_ENABLE_PIN, HIGH);  // enable sending
+    sendMsg (fWrite, msg, sizeof msg);
+    digitalWrite (RS485_ENABLE_PIN, LOW);  // disable sending
+    
+  // receive response. does awaiting a response stop execution?  
+    byte buf [10];
+    byte received = recvMsg (fAvailable, fRead, buf, sizeof buf);
+    if(received != 0 && buf[1] == i){ //if we got a response, and it's the one we expected...
+      internReadings[i-1] = buf[2]; //store the reading.
+      //Wire.write(i, buf[2], 2);
+      Serial.print("Reply from ");
+      Serial.print(i, DEC);
+      Serial.print(": ");
+      Serial.println(buf[2], DEC);
+      probation[i-1] = 0;
+    }
+    else if(received == 0){
+      Serial.print("No Reply from ");
+      Serial.println(i, DEC);
+      probation[i-1] += 1;
+      if(probation[i-1] >= GRACE_PERIOD){
+        Serial.print("Blacklisted a node: ");
+        Serial.println(i, DEC);
+        byte blmsg [] = { 
+          99,
+          0,
+          i    // device id
+        };
+        digitalWrite (RS485_ENABLE_PIN, HIGH);  // enable sending
+        sendMsg (fWrite, blmsg, sizeof blmsg);
+        digitalWrite (RS485_ENABLE_PIN, LOW);  // disable sending
+        deadNodes[i-1] = 1;
+        probation[i-1] = 0;
+      }
+    }
+    digitalWrite (LED_PIN, LOW);
+  } 
+}
+
+//byte msg [] = {
+//   0,  // device 0 (master),
+//   MY_ID, //who sent this
+//   packet  // latest sensor reading
+//};
